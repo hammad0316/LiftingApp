29f5bdc02d4d56ebc71f55cc5d436f6b46872687
je7ov <jeffrey.ovalles@gmail.com>
2018-09-02 21:18:58 -0400

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Lift.create date: Date.today, liftname: 'sBench Pres', ismetric: false, weightlifted: '220', repsperformed: '1', onerm: '1'
